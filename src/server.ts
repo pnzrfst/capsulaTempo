fee21957377bef88a94df3a3ae2be34e48dde629
pnzrfst <jxssxp@gmail.com>
2025-03-03 17:54:15 -0300

    Implementando a logica do cors, para poder acessar minha api pelo meu frontent

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,23 @@
 import fastify from "fastify";
 import { routes } from "./routes";
-import cors from '@fastify/cors';
+import cors from "@fastify/cors";
 
 export const app = fastify();
 
 app.register(routes);
 
 //limita o acesso da api por configuracoes.
-// app.register(cors, {});
+app.register(cors, {
+  // Opções de configuração do CORS
+  origin: "*", // Permite todas as origens (você pode modificar conforme necessário)
+  methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+  allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
+});
 
 //listener pra subir o server numa porta
-app.listen({port: 8080}, (err, address) =>{
-    if(err){
-        console.log(err)
-    }
-    console.log(`Server rodando`);
-})
-
+app.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Server rodando`);
+});
