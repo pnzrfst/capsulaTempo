77e5116dafbe6c46e8a37eed92baeae94fb2e703
pnzrfst <jxssxp@gmail.com>
2025-03-20 17:43:30 -0300

    subindo

--- capsula/src/pages/home/index.jsx
+++ capsula/src/pages/home/index.jsx
@@ -6,13 +6,21 @@ import { useEffect, useState } from "react";
 
 function Home() {
   const [mensagens, setMensagens] = useState([]);
+  const [novaMensagem, setNovaMensagem] = useState("");
 
   async function getMessages() {
     const mensagensFromApi = await api.get("/dashboard");
+    console.log(mensagensFromApi.data);
 
     setMensagens(mensagensFromApi.data);
   }
 
+
+  async function sendMessage() {
+    await api.post("/leaveMessage", {
+      text
+    })
+  }
   useEffect(() => {
     getMessages();
   }, []);
@@ -24,8 +32,14 @@ function Home() {
           <h1>Escreva algo pro futuro: </h1>
           <p>Eu gostaria de ....</p>
 
-          <input type="text" />
-          <button type="button">Enviar</button>
+          <input
+            type="text"
+            value={novaMensagem}
+            onChange={(event) => setNovaMensagem(event.target.value)}
+          />
+          <button type="button" /*onClick={sendMessage}*/>
+            Enviar
+          </button>
         </form>
       </section>
       {mensagens.map((mensagem) => {
